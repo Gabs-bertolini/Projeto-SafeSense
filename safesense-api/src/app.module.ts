761d7fc63d0cbff9a5948a2e6eb83a4a103a8d56
Gabriel <gabrielinfoecia@gmail.com>
2026-03-02 11:48:13 -0300

    feat: module user & device

--- safesense-api/src/app.module.ts
+++ safesense-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DeviceModule } from './device/device.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DeviceModule],
   controllers: [AppController],
   providers: [AppService],
 })
